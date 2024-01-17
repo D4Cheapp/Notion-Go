874b4c412ee63a45b24a2e1be6f004640ed320e2
Scherbakov Ilya <scherbakov720@gmail.com>
2024-01-18 00:57:01 +0300

    Add header component

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useFonts } from 'expo-font';
-import { useCallback } from 'react';
-import {
-  Platform,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import { Client } from '@notionhq/client';
 import * as SplashScreen from 'expo-splash-screen';
+import * as SecureStore from 'expo-secure-store';
+import Header from 'components/Header';
 import { Colors } from 'constants/theme';
 
 void SplashScreen.preventAutoHideAsync();
 
 function App() {
+  const [taskView, setTaskView] = useState<'calendar' | 'list'>('list');
+  const [client, setClient] = useState<Client>();
+  const [authData, setAuthData] = useState<{
+    auth_key: string | null;
+    database_id: string | null;
+  }>();
+
   const [fontsLoaded] = useFonts({
     light: require('./assets/fonts/Roboto-Light.ttf'),
     regular: require('./assets/fonts/Roboto-Regular.ttf'),
@@ -26,47 +30,47 @@ function App() {
     }
   }, [fontsLoaded]);
 
+  const getAuthData = async () => {
+    const auth_key = await SecureStore.getItemAsync('auth_key');
+    const database_id = await SecureStore.getItemAsync('database_id');
+    setAuthData({ auth_key, database_id });
+  };
+
+  const getClient = () => {
+    if (authData?.auth_key && authData?.database_id) {
+      const client = new Client({
+        auth: authData.auth_key,
+      });
+      setClient(client);
+    }
+  };
+
+  const authAttempt = async () => {
+    await getAuthData();
+    getClient();
+  };
+
+  useEffect(() => {
+    void authAttempt();
+  }, []);
+
   if (!fontsLoaded) {
     return null;
   }
 
   return (
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     <SafeAreaView onLayout={onLayoutRootView} style={styles.container}>
-      <View style={styles.header}>
-
-
-        <Text
-          style={{
-            fontFamily: 'bold',
-            color: Colors.seaSalt,
-            textAlign: 'center',
-            fontSize: 30,
-          }}
-        >
-          Notion Todo
-        </Text>
-
-      </View>
-
+      <Header taskView={taskView} setTaskView={setTaskView} authAttempt={authAttempt} />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      backgroundColor: Colors.raisinBlack,
-      paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  },
-  header: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    height: 80,
-    borderBottomColor: Colors.englishViolet,
-    borderWidth: 2,
-    borderStyle: 'solid',
+    flex: 1,
+    backgroundColor: Colors.raisinBlack,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
 });
 
--- constants/theme.ts
+++ constants/theme.ts
@@ -1,7 +1,7 @@
 const Colors = {
   raisinBlack: '#1A1C22',
   englishViolet: '#3D3A50',
-  electricIndigo: '#580EF6',
+  electricIndigo: '#5E29D0',
   seaSalt: '#F7F7F7',
 };
 
